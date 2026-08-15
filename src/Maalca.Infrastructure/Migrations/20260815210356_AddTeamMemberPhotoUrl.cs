36b6d9cc8850eeebe0f05c704d4aa4596a038c37
Ciriaco Pichardo <apichardo@poderjudicial.gob.do>
2026-08-15 17:07:02 -0400

    fix: agregar migracion faltante para TeamMember.PhotoUrl

---
+++ src/Maalca.Infrastructure/Migrations/20260815210356_AddTeamMemberPhotoUrl.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Maalca.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTeamMemberPhotoUrl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PhotoUrl",
+                table: "TeamMembers",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PhotoUrl",
+                table: "TeamMembers");
+        }
+    }
+}
